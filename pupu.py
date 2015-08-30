747671a44020536af69b17d727842306cd289388
Heungsub Lee <sub@subl.ee>
2015-08-30 16:44:08 +0900

    Inspired by @suminb

--- pupu.py
+++ pupu.py
@@ -1,1 +1,1 @@
-import zlib,base64;print(zlib.decompress(base64.b64decode(b'eNrdlDsSBCEIBXNOQcz9D7jBWLv8VoVHNEYO2l1AMRLXlzyL2mQLFgAWGaNXAJLM1QLVBal0PzCX7e3ORpceo+I5VUX0rx3q7PQ/yNWiHhbAWyyiFdKxNdTCVVbDZRalFW7C0Zb7M3x9qFAIbHG3YQ4nm9rZe3wWt/jaZokN4+mVI/4TSHaJ5Sz4KkwsFJM+DN7hozG13YxSd3zf7MAkxKiDGFUkI94U9Axjb3pFQB/VQ5nK')).decode())
+import test;print(test.EXAMPLE)
